e94a8fc2862f03de35b5015b55533e630a5fb90f
Lukas Heddendorp <lu.heddendorp@gmail.com>
2019-01-16 13:27:52 +0100

    Add initial UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatDialog} from '@angular/material';
+import {AboutComponent} from './about/about.component';
+import {NewEntryComponent} from './new-entry/new-entry.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pixelday';
+  function;
+
+  constructor(private _dialog: MatDialog){}
+
+  showAbout() {
+    this._dialog.open(AboutComponent);
+  }
+  newEntry() {
+    this._dialog.open(NewEntryComponent);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule, DomSanitizer} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatDialogModule, MatIconModule, MatIconRegistry, MatToolbarModule} from '@angular/material';
+import {HttpClientModule} from '@angular/common/http';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {AboutComponent} from './about/about.component';
+import { NewEntryComponent } from './new-entry/new-entry.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    NewEntryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatDialogModule,
+    HttpClientModule,
+    FlexLayoutModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [AboutComponent, NewEntryComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(san: DomSanitizer, registry: MatIconRegistry) {
+    registry.addSvgIconSet(san.bypassSecurityTrustResourceUrl('/assets/icons/set.svg'));
+  }
+}
