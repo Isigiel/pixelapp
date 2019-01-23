94ae5642a0b04fded9d1c4b5afee0476b4e07656
Lukas Heddendorp <Isigiel@users.noreply.github.com>
2019-01-23 11:10:02 +0100

    Update 1 (#2)
    This should
    * Fix iPhone layout issues
    * Fix miss configured translations
    * Add basic testing
    * Add a basic desktop layout
    * Fix the language redirect rules
    * Adopt a warm color theme
    * Introduce a range of small improvements
    * switch all scripts from `npm` to `yarn`

--- src/app/new-entry/new-entry.component.spec.ts
+++ src/app/new-entry/new-entry.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NewEntryComponent } from './new-entry.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('NewEntryComponent', () => {
   let component: NewEntryComponent;
@@ -8,7 +9,8 @@ describe('NewEntryComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [NewEntryComponent]
+      declarations: [NewEntryComponent],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   }));
 
